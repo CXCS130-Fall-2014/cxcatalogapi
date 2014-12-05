d5873ab333f212a60c1c837a3d443d32f03e32f4
Shuang Yang <ysflowerdog@gmail.com>
2014-12-04 18:55:51 -0800

    typo

--- service/src/main/java/com/shopzilla/service/shoppingcart/resource/IndexResource.java
+++ service/src/main/java/com/shopzilla/service/shoppingcart/resource/IndexResource.java
@@ -21,6 +21,7 @@ import java.io.File; //for test
 @Path("/")
 @Produces(MediaType.TEXT_HTML)
 public class IndexResource {
+
     @GET
     public IndexView handle() {
         IndexView view = new IndexView("/index.mustache");
@@ -32,18 +33,18 @@ public class IndexResource {
         List<String> worlds = new ArrayList<String>();
 
         // for test
-        /*File folder = new File("src/main/resources/assets/static/img");
+        File folder = new File("src/main/resources/assets/static/img");
 
           File curdir = new File(new File(".").getAbsolutePath());
 
-        File[] listOfFiles = folder.listFiles();*/
+        File[] listOfFiles = folder.listFiles();
 
         //int j = 0;
         //worlds.add(new ArrayList<String>());
 
-        /*for (int i = 0; i < listOfFiles.length; i++) {
+        for (int i = 0; i < listOfFiles.length; i++) {
                 String filePath = "style=background:url(/assets/static/img/" + listOfFiles[i].getName() + ");";
-                worlds.add(filePath);*/
+                worlds.add(filePath);
                 /*worlds.get(j).add(filePath);
                 if (j % 10 == 0) {
                     j++;
