c5e8dec63e434a4586c169277342fa50f57a92ce
cococolov <yuyan_yyhu@hotmail.com>
2014-12-10 20:13:47 -0800

    standalone ranking algorithm script, should be compiled and run alone

---
+++ ranking/src/ranking/main.java
@@ -0,0 +1,11 @@
+package ranking;
+
+/**
+ * Created by mac on 14-11-23.
+ */
+public class main {
+    public static void main(String[] args) {
+        ranking ranking_obj = new ranking();
+        ranking_obj.run();
+    }
+}
